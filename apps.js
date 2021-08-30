05864909b14ec706aec43c1aaa62b6f1e6535c59
Anish-JS <anishjakka@gmail.com>
2021-08-30 14:11:28 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,29 +1,31 @@
-var btnTranslate=document.querySelector('#btn-translate');
+var btnTranslate = document.querySelector("#btn-translate");
 console.log(btnTranslate);
 
-var inputTxt=document.querySelector("#txt-input");
+var inputTxt = document.querySelector("#txt-input");
 console.log(inputTxt);
 
-var outputDiv=document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
 console.log(outputDiv);
 
-var serviceURL="https://api.funtranslations.com/translate/klingon.json";
+var serviceURL = "https://api.funtranslations.com/translate/klingon.json";
 
-function ConstructApi(text){
-    return serviceURL+'?'+'text='+text;
+function ConstructApi(text) {
+  return serviceURL + "?" + "text=" + text;
 }
 
-function errorHandler(error){
-    console.log("error occured: ",error);
+function errorHandler(error) {
+  console.log("error occured: ", error);
 }
 
-function clickEventHandler(text){
-    var text=inputTxt.value;
-    fetch(ConstructApi(text)).then(response => response.json()).then(json =>{ 
-        var translatedText=json.contents.translated;
-        outputDiv.innerText=translatedText;
-
-    }).catch(errorHandler);
+function clickEventHandler(text) {
+  var text = inputTxt.value;
+  fetch(ConstructApi(text))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
 
-btnTranslate.addEventListener("click",clickEventHandler);
+btnTranslate.addEventListener("click", clickEventHandler);
