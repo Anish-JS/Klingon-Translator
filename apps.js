bf73d16cfb982ba3d3fd2a9f5fe0d13fdf17e4f7
Anish Jakka Singaraiah <anishjakka@gmail.com>
2022-01-09 16:07:26 +0530

    update es6+

--- apps.js
+++ apps.js
@@ -1,31 +1,27 @@
-var btnTranslate = document.querySelector("#btn-translate");
+const btnTranslate = document.querySelector("#btn-translate");
 console.log(btnTranslate);
 
-var inputTxt = document.querySelector("#txt-input");
+let inputTxt = document.querySelector("#txt-input");
 console.log(inputTxt);
 
-var outputDiv = document.querySelector("#output");
+let outputDiv = document.querySelector("#output");
 console.log(outputDiv);
 
-var serviceURL = "https://api.funtranslations.com/translate/klingon.json";
+const serviceURL = "https://api.funtranslations.com/translate/klingon.json";
 
-function ConstructApi(text) {
-  return serviceURL + "?" + "text=" + text;
-}
+const ConstructApi = (text) => `${serviceURL}?text=${text}`;
 
-function errorHandler(error) {
-  console.log("error occured: ", error);
-}
+const errorHandler = (error) => console.log("error occured: ", error);
 
-function clickEventHandler(text) {
-  var text = inputTxt.value;
+const clickEventHandler = (text) => {
+  let text = inputTxt.value;
   fetch(ConstructApi(text))
     .then((response) => response.json())
     .then((json) => {
-      var translatedText = json.contents.translated;
+      let translatedText = json.contents.translated;
       outputDiv.innerText = translatedText;
     })
     .catch(errorHandler);
-}
+};
 
 btnTranslate.addEventListener("click", clickEventHandler);
